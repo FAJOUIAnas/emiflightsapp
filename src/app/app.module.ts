0dacab4c0bb8f0bb6acb671c43239e7e48a53f69
AnasFajoui <FAJOUI.ANAS@student.emi.ac.ma>
2022-12-17 20:26:05 +0100

    Planes CRUD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {PlaneService} from "./plane.service";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PlaneService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
